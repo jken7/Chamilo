6ebe6a4012aa32dbe0bf6b0b7f8207a3c9468d38
root <root@ip-172-31-34-246.us-west-2.compute.internal>
2017-04-30 02:13:22 +0000

    nueva url

--- app/config/configuration.php
+++ app/config/configuration.php
@@ -43,7 +43,7 @@ $_configuration['db_manager_enabled'] = false;
  * Directory settings
  */
 // URL to the root of your Chamilo installation, e.g.: http://www.mychamilo.com/
-$_configuration['root_web'] = 'http://campus.jken.pe';
+$_configuration['root_web'] = 'LinuxELB-947233601.us-west-2.elb.amazonaws.com';
 
 // Path to the webroot of system, example: /var/www/
 $_configuration['root_sys'] = '/var/www/html/';
